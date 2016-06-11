2df26780fe18746649e8d7ae561978757a2e5f7f
allensu1994 <workallensu@gmail.com>
2016-06-11 13:57:08 -0400

    /quote works on slack

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,9 @@
 var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  Article = mongoose.model('Article'),
-  http = require('http'),
-  querystring = require('querystring');
+router = express.Router(),
+mongoose = require('mongoose'),
+Article = mongoose.model('Article'),
+http = require('http'),
+querystring = require('querystring');
 
 
 module.exports = function (app) {
@@ -19,21 +19,21 @@ router.get('/', function (req, res, next) {
   //     articles: articles
   //   });
   //
-    return "Hello World";
+  return "Hello World";
   
 });
 
 router.get('/yquote', function (req, res, next) {
-   var postData = querystring.stringify({
+ var postData = querystring.stringify({
    'msg' : 'Hello World!'
-  });
+ });
 
-  var ticker = req.query.text;
-  var options = {
-    hostname: 'finance.yahoo.com',
-    port: 80,
-    path: "/webservice/v1/symbols/"+ticker+"/quote?format=json",
-    method: 'GET'
+ var ticker = req.query.text;
+ var options = {
+  hostname: 'finance.yahoo.com',
+  port: 80,
+  path: "/webservice/v1/symbols/"+ticker+"/quote?format=json",
+  method: 'GET'
     // headers: {
     //   'Content-Type': 'application/x-www-form-urlencoded',
     //   'Content-Length': postData.length
@@ -42,17 +42,10 @@ router.get('/yquote', function (req, res, next) {
 
   var apiRequest = http.request(options, (apiResponse) => {
 
-
     console.log(`STATUS: ${res.statusCode}`);
     console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
 
     apiResponse.setEncoding('utf8');
-    // res.on('data', (chunk) => {
-    //   console.log(`BODY: ${chunk}`);
-    // });
-    // res.on('end', () => {
-    //   console.log('No more data in response.')
-    // })
     var body = '';
     apiResponse.on('data', function(d){
       body += d;
@@ -60,16 +53,16 @@ router.get('/yquote', function (req, res, next) {
     apiResponse.on('end', function(){
       var parsed = JSON.parse(body);
       if (parsed.list.meta.count != 0){
-      fields = parsed.list.resources[0].resource.fields;
-      output = fields.name + " " + fields.symbol + " " + fields.price;
+        fields = parsed.list.resources[0].resource.fields;
+        output = fields.name + " " + fields.symbol + " " + fields.price;
       //console.log(output);
       //output += JSON.stringify(req.query);
       res.send(output);
     }
     else {
-      res.status(404).send({error:"Ticker not found"});
+      res.status(404).send("Ticker not found");
     }
-    });
+  });
   });
 
   apiRequest.on('error', (e) => {
